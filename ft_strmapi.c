b833acfe9cfe8b30e63b782e14a48e453374ea16
pemarti2 <pedromartinezgarcia.dev@gmail.com>
2024-01-23 20:24:31 +0100

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//A cada carácter de la string ’s’, aplica la función ’f’ dando como parámetros el índice de cada carácter dentro de ’s’ y el propio carácter. Genera una nueva string con el resultado del uso sucesivo de ’f’.
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char		*response;
+	char		*presponse;
+	unsigned int	i;
 
+	i = (unsigned int) ft_strlen(s);
+	response = (char *) malloc(sizeof(char) * (i + 1));
+	if (response != NULL)
+	{
+		presponse = response;
+		i = 0;
+		while (*s)
+			*presponse++ = f(i++, *s++);
+		*presponse = '\0';
+	}
+	else
+	{
+		free(response);
+		return (NULL);
+	}
+	return (response);
+}
